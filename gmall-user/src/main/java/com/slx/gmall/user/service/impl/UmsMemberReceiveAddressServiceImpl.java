2e636862864ee4a64bc297c84491e4e1fc920d2e
songliangxiong <1125802247@qq.com>
2020-03-02 01:38:28 +0800

    第二天，项目配置

--- gmall-user/src/main/java/com/slx/gmall/user/service/impl/UmsMemberReceiveAddressServiceImpl.java
+++ gmall-user/src/main/java/com/slx/gmall/user/service/impl/UmsMemberReceiveAddressServiceImpl.java
@@ -5,6 +5,7 @@ import com.slx.gmall.user.mapper.UmsMemberReceiveAddressMapper;
 import com.slx.gmall.user.service.UmsMemberReceiveAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 @Service
@@ -13,9 +14,12 @@ public class UmsMemberReceiveAddressServiceImpl implements UmsMemberReceiveAddre
     UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
     @Override
     public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
-        UmsMemberReceiveAddress u=new UmsMemberReceiveAddress();
-        u.setMemberId(memberId);
-        List<UmsMemberReceiveAddress> list=umsMemberReceiveAddressMapper.select(u);
+//        UmsMemberReceiveAddress u=new UmsMemberReceiveAddress();
+//        u.setMemberId(memberId);
+
+        Example example=new Example(UmsMemberReceiveAddress.class);
+        example.createCriteria().andEqualTo("memberId",memberId);
+        List<UmsMemberReceiveAddress> list=umsMemberReceiveAddressMapper.selectByExample(example);
         return list;
     }
 }
